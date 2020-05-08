5fba35ab62fdf1a4f3f931510a15bd947fc13312
rvsgltn <rvsgltkn@hotmail.com>
2020-05-07 19:53:44 -0700

    cleaning unnecessary files

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,2 @@
-from flask import Blueprint
-
 from .main.controller.service_controller import api as service_bp
 
-# blueprint = Blueprint('api', __name__)
-#
-# api = Api(blueprint,
-#           title='FLASK TEST PROJECT',
-#           version='1.0',
-#           description='flask web service'
-#           )
-#
-# api.add_namespace(service_ns)
